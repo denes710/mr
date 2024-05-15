2a3f87f51a3a73451393184256f7ff0476725871
Denes Fekete <denes710@gmail.com>
2024-05-15 21:35:59 +0200

    progress: coordinator and worker programs added

---
+++ cmd/mrcoordinator.go
@@ -0,0 +1,32 @@
+package main
+
+//
+// start the coordinator process, which is implemented
+// in ../coordinator/coordinator.go
+//
+// go run mrcoordinator.go pg*.txt
+//
+// Please do not change this file.
+//
+
+import (
+	"fmt"
+	"os"
+	"time"
+
+	mrcoordinator "github.com/denes710/mr/coordinator"
+)
+
+func main() {
+	if len(os.Args) < 2 {
+		fmt.Fprintf(os.Stderr, "Usage: mrcoordinator inputfiles...\n")
+		os.Exit(1)
+	}
+
+	m := mrcoordinator.MakeCoordinator(os.Args[1:], 10)
+	for m.Done() == false {
+		time.Sleep(time.Second)
+	}
+
+	time.Sleep(time.Second)
+}
